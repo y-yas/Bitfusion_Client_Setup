c173e271a0110ded227b57defffd94de20ab85ab
y-yas <55268781+y-yas@users.noreply.github.com>
2021-02-12 14:58:23 +0900

    Update setup_ubuntu.sh

--- setup_ubuntu.sh
+++ setup_ubuntu.sh
@@ -6,9 +6,13 @@ sudo apt install -y wget
 mkdir ~/bitfusion
 cd ~/bitfurion
 
-# bitfusion version 2.5.1.-13
+#bitfusion version 2.5.1.-13
 wget https://packages.vmware.com/bitfusion/ubuntu/18.04/bitfusion-client-ubuntu1804_2.5.1-13_amd64.deb
 sudo apt-get install -y ./bitfusion-client-ubuntu1804_2.5.1-13_amd64.deb
 
 #version確認
 bitfusion version
+
+#不要ファイル削除
+cd && rm -fr ./bitfusion
+ 
