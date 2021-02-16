3f28dc7af133155cf7591914108d3273a02c0ed3
y-yas <55268781+y-yas@users.noreply.github.com>
2021-02-16 15:13:58 +0900

    Update setup_ubuntu.sh

--- setup_ubuntu.sh
+++ setup_ubuntu.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+
+#distribution=$(. /etc/os-release;echo $ID$VERSION_ID)
+distribution=$(. /etc/os-release;echo $VERSION_ID)
+
 sudo apt-get update
 sudo apt install -y wget open-vm-tools
 
@@ -7,7 +11,7 @@ mkdir ~/bitfusion
 cd ~/bitfusion
 
 #bitfusion version 2.5.1.-13
-wget https://packages.vmware.com/bitfusion/ubuntu/18.04/bitfusion-client-ubuntu1804_2.5.1-13_amd64.deb
+wget https://packages.vmware.com/bitfusion/ubuntu/$distribution/bitfusion-client-ubuntu1804_2.5.1-13_amd64.deb
 sudo apt-get install -y ./bitfusion-client-ubuntu1804_2.5.1-13_amd64.deb
 
 #version確認
